6ec1edc7547d5378862b38b6766e2f27b2edcb8c
tanveerm176 <tanveerm176@gmail.com>
2024-04-28 15:48:20 -0400

    added title and subtitle with css

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 const App = () => {
+  return(
+    <div className='App'>
+      <h1>Coffee Places in NYC</h1>
+      <h2>Here are some of my favorite coffee places in NYC!</h2>
+    </div>
+  )
 }
 
 export default App
